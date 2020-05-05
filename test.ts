1313a4662eab035573ce24f959b6267734c51673
JD Cargile <jcargile@control4.com>
2020-05-05 11:18:02 -0600

    Add buildTime to notification

---
+++ test.ts
@@ -0,0 +1,23 @@
+import moment from "moment-timezone";
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit({
+	auth: "865ab938ca7dd1728b92ba882cbd0fd8d5932cc0"
+});
+//const runParams = { owner: "control4", repo: "dealer-portal", ref: "develop", run_id: 95794734 };
+//const workflowRun = octokit.actions.getWorkflowRun(runParams).then((data: any) => console.log(data));
+
+octokit.request('GET /repos/:owner/:repo/actions/runs/:runId/jobs', {
+	owner: "control4",
+	repo: "dealer-portal",
+	runId: 95794734
+}).then((response: { data: any; }) => {
+	const jobSteps = response.data.jobs[0].steps
+	jobSteps.forEach((job: any) => {
+		if (job.name === "Build") {
+			var started_at = moment(job.started_at);
+			var completed_at = moment(job.completed_at);
+			var buildTime = moment.duration(completed_at.diff(started_at));
+			console.log(`${buildTime.minutes()} minutes ${buildTime.seconds()} seconds`);
+		}
+	});
+});
